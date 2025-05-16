b359bb6f41212f8838d7bb9fc2eb9416cbdaa539
patel muhammad <148962037+patel192@users.noreply.github.com>
2025-05-16 22:48:12 +0530

    Chat SIdebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import { ChatSidebar } from './Components/ChatSidebar'
 import Navbar from './Components/Navbar'
 
 function App() {
   return (
     <>
     <Navbar/>
+    <ChatSidebar/>
     </>
     
   )
