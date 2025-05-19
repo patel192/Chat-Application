3207ce2331d0003757beb957a9ea79dbd8b7156c
patel muhammad <148962037+patel192@users.noreply.github.com>
2025-05-19 21:04:46 +0530

    Props practice React

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 
 import './App.css'
-import { ChatSidebar } from './Components/ChatSidebar'
-import Navbar from './Components/Navbar'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import Navbar from './Components/Navbar';
+
 
 function App() {
+  const {register,handleSubmit}= useForm();
+  const [obj, setobj] = useState({name:"",age:"",income:"",gender:""})
+  const SubmitHandler = (data) => {
+    setobj(data)
+    console.log(obj)
+  }
+  const [show, setshow] = useState(true)
+  const handleToggle = () =>{
+    setshow(!show)
+  }
   return (
     <>
-    <Navbar/>
-    <ChatSidebar/>
+    <form action="" onSubmit={handleSubmit(SubmitHandler)}>
+
+    <input type="text" placeholder='Name' {...register("name")} />
+    <input type="text" placeholder='Age' {...register("age")} />
+    <input type="text" placeholder='Income' {...register("income")} />
+    <input type="text" placeholder='Gender' {...register("gender")} />
+    <input type="submit" value="submit" />
+    </form>
+    <button onClick={handleToggle} type='button'>Toggle</button>
+    {show ? <Navbar text={obj}/> : null}
     </>
-    
   )
 }
 
