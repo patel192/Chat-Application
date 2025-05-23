cddab6fa4c82dcf273d1a079a8a6086d646bc09c
patel muhammad <148962037+patel192@users.noreply.github.com>
2025-05-23 20:37:51 +0530

    navbar for the chat appilcation

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,11 @@
-
-import './App.css'
-import { useState } from 'react'
-import { useForm } from 'react-hook-form'
-import Navbar from './Components/Navbar';
-
-
+import "./App.css";
+import { Navbar } from "./Components/Home/Navbar";
 function App() {
-  const {register,handleSubmit}= useForm();
-  const [obj, setobj] = useState({name:"",age:"",income:"",gender:""})
-  const SubmitHandler = (data) => {
-    setobj(data)
-    console.log(obj)
-  }
-  const [show, setshow] = useState(true)
-  const handleToggle = () =>{
-    setshow(!show)
-  }
   return (
     <>
-    <form action="" onSubmit={handleSubmit(SubmitHandler)}>
-
-    <input type="text" placeholder='Name' {...register("name")} />
-    <input type="text" placeholder='Age' {...register("age")} />
-    <input type="text" placeholder='Income' {...register("income")} />
-    <input type="text" placeholder='Gender' {...register("gender")} />
-    <input type="submit" value="submit" />
-    </form>
-    <button onClick={handleToggle} type='button'>Toggle</button>
-    {show ? <Navbar text={obj}/> : null}
+      <Navbar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-const Navbar = ({text}) => {
-  return (
-   <>
-     <h1>{JSON.stringify(text)}</h1>
-   </>
-  );
-};
-
-export default Navbar;
