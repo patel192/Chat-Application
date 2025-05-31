612890e6fe7d0b150cf1ef3f1a04d679da99bb64
patel muhammad <148962037+patel192@users.noreply.github.com>
2025-05-31 12:14:12 +0530

    signup toaster

--- src/Components/Home/Signup.jsx
+++ src/Components/Home/Signup.jsx
@@ -2,25 +2,23 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useForm } from "react-hook-form";
-import { ToastContainer,toast } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 export const Signup = () => {
   const { register, handleSubmit } = useForm();
   const navigate = useNavigate();
-  const notify = () =>{
-    toast.success("User Created Successfully!",{
-      position:"top-center"
-    })
-  }
+
   const SubmitHandler = async (data) => {
     try {
       const res = await axios.post("/user", data);
       if (res.status === 201) {
-        alert("User Created Succefully");
+        toast.success("User Created Successfully!", {
+          position: "top-center",
+        });
+
         console.log(res);
         setTimeout(() => {
           navigate("/login");
         }, 2000);
-        <ToastContainer/>
       } else {
         alert("User Not Created");
       }
@@ -30,6 +28,7 @@ export const Signup = () => {
   };
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <ToastContainer />;
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           Create your ChatApp account
